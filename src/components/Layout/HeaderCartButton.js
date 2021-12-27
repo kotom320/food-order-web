11e270065902145cef977e4c332ee95f6866ddd9
DongWook Ko <kotom320@gmail.com>
2021-12-27 15:28:24 +0900

    add header

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import classes from './HeaderCartButton.module.css'
+import cartIcon from '../../assets/cart.png'
+const HeaderCartButton = (props) =>{
+    return <button className={classes.button}>
+<span className={classes.icon}>
+    <img src={cartIcon}></img>
+</span>
+<span>
+    Your Cart
+</span>
+<span className={classes.badge}>
+    3
+</span>
+
+    </button>
+}
+export default HeaderCartButton
